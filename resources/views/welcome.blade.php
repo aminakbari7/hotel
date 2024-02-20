3cda877d48b535632c18224e19dae4ba11001589
aminakbari7 <aminakbari733@gmail.com>
2024-02-20 15:48:58 +0330

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -35,79 +35,8 @@
         </style>
     </head>
     <body class="antialiased">
-            <div class="container-xxl bg-white p-0">
-                <!-- Spinner Start -->
-                <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
-                    <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
-                        <span class="sr-only">Loading...</span>
-                    </div>
-                </div>
-                <!-- Spinner End -->
-        
-                <!-- Header Start -->
-                <div class="container-fluid bg-dark px-0">
-                    <div class="row gx-0">
-                        <div class="col-lg-3 bg-dark d-none d-lg-block">
-                            <a href="index.html" class="navbar-brand w-100 h-100 m-0 p-0 d-flex align-items-center justify-content-center">
-                                <h1 class="m-0 text-primary text-uppercase">Hotelier</h1>
-                            </a>
-                        </div>
-                        <div class="col-lg-9">
-                            <div class="row gx-0 bg-white d-none d-lg-flex">
-                                <div class="col-lg-7 px-5 text-start">
-                                    <div class="h-100 d-inline-flex align-items-center py-2 me-4">
-                                        <i class="fa fa-envelope text-primary me-2"></i>
-                                        <p class="mb-0">info@example.com</p>
-                                    </div>
-                                    <div class="h-100 d-inline-flex align-items-center py-2">
-                                        <i class="fa fa-phone-alt text-primary me-2"></i>
-                                        <p class="mb-0">+012 345 6789</p>
-                                    </div>
-                                </div>
-                                <div class="col-lg-5 px-5 text-end">
-                                    <div class="d-inline-flex align-items-center py-2">
-                                        <a class="me-3" href=""><i class="fab fa-facebook-f"></i></a>
-                                        <a class="me-3" href=""><i class="fab fa-twitter"></i></a>
-                                        <a class="me-3" href=""><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telegram" viewBox="0 0 16 16">
-                                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.287 5.906q-1.168.486-4.666 2.01-.567.225-.595.442c-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294q.39.01.868-.32 3.269-2.206 3.374-2.23c.05-.012.12-.026.166.016s.042.12.037.141c-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8 8 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629q.14.092.27.187c.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.4 1.4 0 0 0-.013-.315.34.34 0 0 0-.114-.217.53.53 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09"/>
-                                          </svg></a>
-                                        <a class="me-3" href=""><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-instagram" viewBox="0 0 16 16">
-                                            <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334"/>
-                                          </svg></a>
-                                        <a class="" href=""><i class="fab fa-youtube"></i></a>
-                                    </div>
-                                </div>
-                            </div>
-                            <nav class="navbar navbar-expand-lg bg-dark navbar-dark p-3 p-lg-0">
-                                <a href="index.html" class="navbar-brand d-block d-lg-none">
-                                    <h1 class="m-0 text-primary text-uppercase">Hotelier</h1>
-                                </a>
-                                <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-                                    <span class="navbar-toggler-icon"></span>
-                                </button>
-                                <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
-                                    <div class="navbar-nav mr-auto py-0">
-                                        <a href="index.html" class="nav-item nav-link active">Home</a>
-                                        <a href="about.html" class="nav-item nav-link">About</a>
-                                        <a href="service.html" class="nav-item nav-link">Services</a>
-                                        <a href="room.html" class="nav-item nav-link">Rooms</a>
-                                        <div class="nav-item dropdown">
-                                            <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
-                                            <div class="dropdown-menu rounded-0 m-0">
-                                                <a href="booking.html" class="dropdown-item">Booking</a>
-                                                <a href="team.html" class="dropdown-item">Our Team</a>
-                                                <a href="testimonial.html" class="dropdown-item">Testimonial</a>
-                                            </div>
-                                        </div>
-                                        <a href="contact.html" class="nav-item nav-link">Contact</a>
-                                    </div>
-                                </div>
-                            </nav>
-                        </div>
-                    </div>
-                </div>
-                <!-- Header End -->
-    
+        <div class="container-xxl bg-white p-0">
+        @include('layouts.nav')
                 <!-- Carousel Start -->
                 <div class="container-fluid p-0 mb-5">
                     <div id="header-carousel" class="carousel slide" data-bs-ride="carousel">
