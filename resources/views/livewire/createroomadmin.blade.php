fe74279b52469dc17939020e44ecbb29a4d30492
aminakbari7 <aminakbari733@gmail.com>
2024-02-21 12:02:14 +0330

    Update createroomadmin.blade.php

--- resources/views/livewire/createroomadmin.blade.php
+++ resources/views/livewire/createroomadmin.blade.php
@@ -1,17 +1,24 @@
 <div class="card-body" dir="rtl">
     <h4 class="card-title">add room</h4>
-    <form class="forms-sample">
+    @if (session()->exists('msg'))
+    <div x-data="{show: true}" x-init="setTimeout(() => show = false, 1000)" x-show="show">
+        <div class="alert alert-success">
+            {{ session('msg') }}
+        </div>
+    </div>
+  @endif
+    <form class="forms-sample" wire:submit="save">
       <div class="form-group">
         <label for="exampleInputName1">size</label>
-        <input name="size" type="text" class="form-control" id="exampleInputName1" placeholder="size">
+        <input wire:model="size" name="size" type="text" class="form-control" id="exampleInputName1" placeholder="size">
       </div>
       <div class="form-group">
         <label for="exampleInputEmail3">price</label>
-        <input name="price" type="text" class="form-control" id="exampleInputEmail3" placeholder="price">
+        <input wire:model="price" name="price" type="text" class="form-control" id="exampleInputEmail3" placeholder="price">
       </div>
       <div class="form-group">
         <label for="exampleSelectGender">child</label>
-        <select class="form-control" id="exampleSelectGender">
+        <select wire:model="child" class="form-control" id="exampleSelectGender">
           <option>1</option>
           <option>2</option>
           <option>3</option>
@@ -22,16 +29,17 @@
       <br>
       <div class="form-group">
         <div class="input-group col-xs-12" dir="ltr">
-          <input type="text" class="form-control file-upload-info" disabled="" placeholder="Upload Image">
+          <input  type="text" class="form-control file-upload-info" disabled="" placeholder="Upload Image">
           <span class="input-group-append">
-            <input type="file" class="file-upload-browse btn btn-warning" type="button"> </button>
+            <input wire:model="image" type="file" class="file-upload-browse btn btn-warning" type="button"> </button>
           </span>
         </div>
       </div>
       <div class="form-group">
         <label for="exampleTextarea1">Textarea</label>
-        <textarea class="form-control" id="exampleTextarea1" rows="4"></textarea>
+        <textarea  wire:model="description" class="form-control" id="exampleTextarea1" rows="4"></textarea>
       </div>
+     
       <button class="btn btn-success "> save </button>
       <button class="btn btn-light">Cancel</button>
     </form>
