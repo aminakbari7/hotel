dce127cdce9d2c2156512d58e9f741fbb6f4b6d3
aminakbari7 <aminakbari733@gmail.com>
2024-02-21 11:44:21 +0330

    Update createroom.blade.php

--- resources/views/admin/createroom.blade.php
+++ resources/views/admin/createroom.blade.php
@@ -37,48 +37,9 @@
 <body>
     <div class="container-xxl bg-white p-0" >
         @include('layouts.nav')
-        <div class="card-body" dir="rtl">
-            <h4 class="card-title">add room</h4>
-            <form class="forms-sample">
-              <div class="form-group">
-                <label for="exampleInputName1">size</label>
-                <input name="size" type="text" class="form-control" id="exampleInputName1" placeholder="size">
-              </div>
-              <div class="form-group">
-                <label for="exampleInputEmail3">price</label>
-                <input name="price" type="text" class="form-control" id="exampleInputEmail3" placeholder="price">
-              </div>
-              <div class="form-group">
-                <label for="exampleSelectGender">child</label>
-                <select class="form-control" id="exampleSelectGender">
-                  <option>1</option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                  <option>5</option>
-                </select>
-              </div>
-              <br>
-              <div class="form-group">
-                <div class="input-group col-xs-12" dir="ltr">
-                  <input type="text" class="form-control file-upload-info" disabled="" placeholder="Upload Image">
-                  <span class="input-group-append">
-                    <input type="file" class="file-upload-browse btn btn-warning" type="button"> </button>
-                  </span>
-                </div>
-              </div>
-              <div class="form-group">
-                <label for="exampleTextarea1">Textarea</label>
-                <textarea class="form-control" id="exampleTextarea1" rows="4"></textarea>
-              </div>
-              <button class="btn btn-success "> save </button>
-              <button class="btn btn-light">Cancel</button>
-            </form>
-          </div>
-
+        @livewire('Createroomadmin')
     </div>
 
-    <!-- JavaScript Libraries -->
     <!-- JavaScript Libraries -->
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
