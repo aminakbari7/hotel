3861c5c9163d4a2cfc7793c223cc8edd7a94600b
aminakbari7 <aminakbari733@gmail.com>
2024-02-22 10:43:31 +0330

    Update roomlivewire.blade.php

--- resources/views/livewire/roomlivewire.blade.php
+++ resources/views/livewire/roomlivewire.blade.php
@@ -26,7 +26,7 @@
                 </div>
                 <p class="text-body mb-3">{{ $room->description }}</p>
                 <div class="d-flex justify-content-between">
-                    <a class="btn btn-sm btn-primary rounded py-2 px-4" href="">View Detail</a>
+                    <a class="btn btn-sm btn-primary rounded py-2 px-4" href="{{route('room.showsingle' ,$room->id)}}">View Detail</a>
                     <a class="btn btn-sm btn-dark rounded py-2 px-4" href="">Book Now</a>
                 </div>
             </div>
