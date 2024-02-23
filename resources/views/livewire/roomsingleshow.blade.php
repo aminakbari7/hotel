f2d344d0bcde559b53d6e61cda7f6a8fc9da3d47
aminakbari7 <aminakbari733@gmail.com>
2024-02-23 13:14:59 +0330

    Update roomsingleshow.blade.php

--- resources/views/livewire/roomsingleshow.blade.php
+++ resources/views/livewire/roomsingleshow.blade.php
@@ -1,28 +1,36 @@
 <div class="col-lg-6">
     <div class="wow fadeInUp" data-wow-delay="0.2s">
-        <form>
-            {{ $room->size }}
+        <form wire:submit="save">
+            
             <div class="row g-3">
                 <div class="col-md-6">
+                {{ $room->price }}
+            </div>
+
+                <div class="col-md-6">
+                {{ $room->size }}
+                </div>
+                <div class="col-md-6">
                     <div class="form-floating date" id="date3" data-target-input="nearest">
-                        <input type="text" class="form-control datetimepicker-input" id="checkin" placeholder="Check In" data-target="#date3" data-toggle="datetimepicker" />
+                        <input wire:model="start" type="date" class="form-control datetimepicker-input" placeholder="Check In"/>
                         <label for="checkin">Check In</label>
-                    </div>
+                </div>
                 </div>
                 <div class="col-md-6">
                     <div class="form-floating date" id="date4" data-target-input="nearest">
-                        <input type="text" class="form-control datetimepicker-input" id="checkout" placeholder="Check Out" data-target="#date4" data-toggle="datetimepicker" />
+                        <input wire:model="end" type="date" class="form-control datetimepicker-input"  placeholder="Check Out"  />
                         <label for="checkout">Check Out</label>
                     </div>
                 </div>
                 <div class="col-12">
                     <div class="form-floating">
-                        <textarea class="form-control" placeholder="Special Request" id="message" style="height: 100px"></textarea>
-                        <label for="message">Special Request</label>
+                        <h1>start={{ $start }}</h1>
+                        <h1>end={{ $end }}</h1>
+                        <h1>day={{ $day }}</h1>
                     </div>
                 </div>
                 <div class="col-12">
-                    <button class="btn btn-primary w-100 py-3" type="submit">Book Now</button>
+                    <button class="btn btn-success w-100 py-3" type="submit"><strong style="color: black">Book Now</strong></button>
                 </div>
             </div>
         </form>
