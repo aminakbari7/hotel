cca4fb9255d3e154fe59593cf5201de389c73384
aminakbari7 <aminakbari733@gmail.com>
2024-02-27 12:04:26 +0330

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {return view('welcome');});
+Route::get('/contact', function () {return view('contact');})->name('contact');
+
 Route::get('roomlivewire',Roomlivewire::class);
 Route::get('homelivewire',Homelivewire::class);
 
