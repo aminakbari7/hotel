730a287edeed681193dfd7b6a4fc8a7df80782f0
aminakbari7 <aminakbari733@gmail.com>
2024-02-28 08:10:39 +0330

    Update Adminbookingshow.php

--- app/Livewire/Adminbookingshow.php
+++ app/Livewire/Adminbookingshow.php
@@ -1,11 +1,19 @@
 <?php
 
 namespace App\Livewire;
-
+use App\Models\booking;
+use App\Models\room;
+use Livewire\WithFileUploads;
+use Illuminate\Support\Facades\Session;
 use Livewire\Component;
 
 class Adminbookingshow extends Component
 {
+    public $bookings;
+    public function mount()
+    {
+        $this->bookings=booking::all();
+    }
     public function render()
     {
         return view('livewire.adminbookingshow');
