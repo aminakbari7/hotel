8d0164005a3bdd1421761dacdb1d4ae9a3cb8e90
aminakbari7 <aminakbari733@gmail.com>
2024-02-28 12:19:59 +0330

    Update Adminbookingshow.php

--- app/Livewire/Adminbookingshow.php
+++ app/Livewire/Adminbookingshow.php
@@ -11,6 +11,30 @@ class Adminbookingshow extends Component
 {
     public $bookings;
     public $keybooking=-1,$ubooking=-1;
+    public $newstatus="";
+
+
+    public function edit($id)
+    {
+        $this->ubooking=$id;
+        $this->keybooking=1;
+    }
+ 
+    public function update()
+    {
+        if($this->newstatus)
+        {
+
+            $temp=booking::find($this->ubooking);
+            $temp->status=$this->newstatus;
+            $temp->save();
+            $this->ubooking=-1;
+            $this->keybooking=-1;
+            $this->newstatus="";
+        }
+        $this->mount();
+
+    }
     public function mount()
     {
         $this->bookings=booking::all();
