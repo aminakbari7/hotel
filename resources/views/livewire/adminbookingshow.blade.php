38a8b853b77eb0d99269c888a8102d0501d6e9f9
aminakbari7 <aminakbari733@gmail.com>
2024-02-28 08:27:27 +0330

    Update adminbookingshow.blade.php

--- resources/views/livewire/adminbookingshow.blade.php
+++ resources/views/livewire/adminbookingshow.blade.php
@@ -39,17 +39,22 @@
                     <td>{{ $booking->start }}</td>
                     <td>{{ $booking->end }}</td>
                     <td>{{ $booking->day }}</td>
-                    <td>{{ $booking->tootalprice }}</td>
-
-
+                    <td>{{ $booking->totalprice }}</td>
 
                     <td>
                         <a  class="btn btn-danger  text-center "><svg width="25" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path>
                       </svg></a>
-                        <a  class="btn btn-warning  text-center "><svg width="25" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <a  class="btn btn-warning  text-center ">
+                        <svg width="25" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path>
-                      </svg></a>
+                         </svg>
+                    </a>
+                    <a  class="btn btn-success  text-center ">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425z" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+                          </svg>
+                    </a>
                     </td>
                 </tr>
                 @endif
