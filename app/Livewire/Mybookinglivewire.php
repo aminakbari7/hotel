54662b6a8011ae0143c70b000bc81c75b0f898c1
aminakbari7 <aminakbari733@gmail.com>
2024-03-02 15:13:15 +0330

    Update Mybookinglivewire.php

--- app/Livewire/Mybookinglivewire.php
+++ app/Livewire/Mybookinglivewire.php
@@ -2,18 +2,30 @@
 
 namespace App\Livewire;
 
+
 use Livewire\Component;
 use App\Models\booking;
+use Livewire\Attributes\On;
 use App\Models\room;
 use Livewire\WithFileUploads;
 use Illuminate\Support\Facades\Session;
 class Mybookinglivewire extends Component
 {
     public $size=0;
+    public $booking;
+   
+    #[On('listcheck')]
+    public function updatelist()
+    {
+        $this->booking=booking::select()->where('user_id', auth()->user()->id)->get();
+        $this->size=$this->booking->count();
+        return view('livewire.mybookinglivewire');
+    }
+   
     public function render()
     {
-        $booking=booking::select()->where('user_id', auth()->user()->id)->get();
-        $this->size=$booking->count();
-        return view('livewire.mybookinglivewire',['booking'=> $booking]);
+        $this->booking=booking::select()->where('user_id', auth()->user()->id)->get();
+        $this->size=$this->booking->count();
+        return view('livewire.mybookinglivewire');
     }
 }
