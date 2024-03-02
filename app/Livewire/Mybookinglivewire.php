b8f9d5938c6fde8d796f01c56d998398e49657e5
aminakbari7 <aminakbari733@gmail.com>
2024-03-02 10:47:03 +0330

    Update Mybookinglivewire.php

--- app/Livewire/Mybookinglivewire.php
+++ app/Livewire/Mybookinglivewire.php
@@ -9,9 +9,11 @@ use Livewire\WithFileUploads;
 use Illuminate\Support\Facades\Session;
 class Mybookinglivewire extends Component
 {
+    public $size=0;
     public function render()
     {
-        $booking=booking::select()->where('user_id', auth()->user()->id);
+        $booking=booking::select()->where('user_id', auth()->user()->id)->get();
+        $this->size=$booking->count();
         return view('livewire.mybookinglivewire',['booking'=> $booking]);
     }
 }
